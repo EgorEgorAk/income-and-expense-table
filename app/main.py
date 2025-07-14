0ee1f42a5ea24c18034ba8d9e97d6a8872b9ad13
osakasonyboy@gmail.com <sensh0god@gmail.com>
2025-07-14 17:53:02 +0300

    feature: new proj structure

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from openpyxl import Workbook
+from app.xlsx_tools.tables import draw_tables
+
+
+def main():
+  workBook = Workbook()
+  sheet = workBook.active
+  tablePath = "files/table_test.xlsx"
+
+  draw_tables(sheet, tablePath)
+  workBook.save(tablePath)
+
+if __name__ == "__main__":
+  main()
